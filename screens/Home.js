2406fd6fd33afb4f46fc543c9f803541c1d0418d
Chathuranga97lj <chathuit97@gmail.com>
2022-04-08 12:50:01 +0530

    add screens for app

---
+++ screens/Home.js
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { StyleSheet } from 'react-native'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 30
+    }
+})
+
+export default Home
